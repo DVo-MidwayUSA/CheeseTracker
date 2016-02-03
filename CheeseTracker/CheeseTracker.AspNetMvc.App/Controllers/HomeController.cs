8c31e47b4083ebdfd38689500a08e53413131078
Daniel Vo <dvo@midwayusa.com>
2016-02-02 22:05:44 -0600

    Added missing files

---
+++ CheeseTracker/CheeseTracker.AspNetMvc.App/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CheeseTracker.AspNetMvc.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
